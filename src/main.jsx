72b0e5afdb51073f7061bbb5fe8c92f389c89c66
rafa-ken <rafaelkenmiyamoto@gmail.com>
2025-10-08 10:29:02 -0300

    commit auth0

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 createRoot(document.getElementById('root')).render(
   <Auth0Provider
-      domain="dev-vlbhbshl7b1pxe8e.us.auth0.com"
-      clientId="eo8WNkaTwrcGujuCMSkRZG3yMYuQvXTy"
+      domain="dev-hw1z84r1ccvnlcxr.us.auth0.com"
+      clientId="kfjb67TsHPqqza6eJ87tJQAkgLdAIAsd"
       authorizationParams={{
-        audience: "https://dev-vlbhbshl7b1pxe8e.us.auth0.com/api/v2/",
+        audience: "https://dev-hw1z84r1ccvnlcxr.us.auth0.com/api/v2/",
         redirect_uri: window.location.origin
       }}
     >
